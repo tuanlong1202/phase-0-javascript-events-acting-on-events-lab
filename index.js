5d299144c027c6c1930e6e504b55c64b92eb43a5
tuanlong1202 <tuanlong1202@gmail.com>
2021-03-26 13:06:01 -0400

    Phase 0 javascript events acting on events lab DONE

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+
+const dodger = document.getElementById('dodger');
+dodger.style.backgroundColor = "#FF69B4" ;
+dodger.style.bottom = "0px";
+
+document.addEventListener("keydown", function(event) {
+    if (event.key == "ArrowLeft") {
+        moveDodgerLeft() ;
+        //console.log("Left");
+    } else if (event.key == "ArrowRight") {
+        moveDodgerRight() ;
+        //console.log("Right");
+    }
+});
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "") ;
+    const left = parseInt(leftNumbers, 10) ;
+
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px` ;
+    }
+}
